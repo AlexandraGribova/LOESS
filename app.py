6e9a882f3e7745ec555443b9f1971f385c1e3392
Грибова Александра Романовна <98461789+AlexandraGribova@users.noreply.github.com>
2023-05-06 21:45:34 +0700

    multy function

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def create_plot(xx, yy, x_new, y_new):
     size = (w * dpi, h * dpi)  # canvas size
     t = np.arange(0, 3, .01)
     area = fig.add_subplot(111)
-    area.plot(x_new[0], y_new, '-', label='LOESS with param')
+    for i in range(len(x_new)):
+        area.plot(x_new[i], y_new[i], '-', label='LOESS with param')
     area.plot(xx[0], yy, 'o', label='Выборка')
     area.legend()
     return fig
@@ -42,23 +43,29 @@ def app():
     #-- Самая важная часть, второй раз я это не напишу --#
     def create_row(row_counter):
         row = [sg.pin(
-            sg.Col([[sg.Text('Число параметров\t', pad=(15, 0)), sg.Spin(number, 1, key=('-PARAM_NUM_-', row_counter), readonly=True, size=4, enable_events=True)],
-                   [sg.Text('Степень полинома\t', pad=(15, 0)), sg.Spin(number, 2, key=('-DEGREE_-', row_counter), readonly=True, size=4, enable_events=True)],
-                   [sg.Text('Размер окна\t', pad=(15, 0)), sg.Spin(part, 0.5, key=('-WIN_SIZE_-', row_counter), readonly=True, size=4, enable_events=True)],
-                   [sg.Button('Удалить', key=('-DEL_INPUT-', row_counter))]],
-                   key=('-ROW-', row_counter)))]
+            sg.Col([[sg.Text('Число параметров\t', pad=((15,5), (15,0))),
+                     sg.Spin(number, 1, key=('-PARAM_NUM_-', row_counter), pad=(15, (15,0)), readonly=True, size=4, enable_events=True)],
+                   [sg.Text('Степень полинома\t', pad=((15,5), 5)),
+                    sg.Spin(number, 2, key=('-DEGREE_-', row_counter), pad=(15, 5), readonly=True, size=4, enable_events=True)],
+                   [sg.Text('Размер окна\t', pad=((15,5), 0)),
+                    sg.Spin(part, 0.5, key=('-WIN_SIZE_-', row_counter), pad=(15, 0), readonly=True, size=4, enable_events=True)],
+                   [sg.Button('Удалить', pad=(15,(10,10)), key=('-DEL_INPUT-', row_counter))]],
+                   key=('-ROW-', row_counter), element_justification='c'))]
         return row
 
     # -- Главное окно --#
     layout = [[sg.Menubar(menu_def, tearoff=False)],
               [sg.Text('Выберите файл с выборкой')],
               [sg.Text('Файл: '), sg.InputText(), sg.FileBrowse('Выбрать файл')],
-              [sg.Column([create_row(1)], k='-ROW_PANEL-')],
+              [sg.Column([create_row(1)], k='-ROW_PANEL1-', vertical_alignment='top'),
+               sg.Column([[]], k='-ROW_PANEL2-', vertical_alignment='top')],
               [sg.Button('Ввести дополнительный параметор', key='-ADD_INPUT-')],
               [sg.Button('Ввод', key='-INPUT-'), sg.Button('Выход', key='-CANCEL-')]]
     window = sg.Window('Имя окна', layout, finalize=True)
+    window[('-ROW-',1)].Widget.configure(borderwidth=1, relief=sg.DEFAULT_FRAME_RELIEF)
     frame = 1
     block_number = 1
+    active_windows = [1]
     #-- Цикл для обработки "событий" и получения "значений" входных данных --#
     while True:
         event, values = window.read()
@@ -69,12 +76,18 @@ def app():
             if block_number < 4:
                 block_number += 1
                 frame += 1
-                window.extend_layout(window['-ROW_PANEL-'], [create_row(frame)])
+                if block_number%2 == 0:
+                    window.extend_layout(window['-ROW_PANEL2-'], [create_row(frame)])
+                else:
+                    window.extend_layout(window['-ROW_PANEL1-'], [create_row(frame)])
+                window[('-ROW-', frame)].Widget.configure(borderwidth=1, relief=sg.DEFAULT_FRAME_RELIEF)
+                active_windows.append(frame)
         if event[0] == '-DEL_INPUT-':
             if block_number > 1:
                 block_number -= 1
                 print(event[1])
                 window[('-ROW-', event[1])].update(visible=False)
+                active_windows.remove(event[1])
         if event == '-INPUT-':
             if values[1]:
                 file = values[1]
@@ -82,23 +95,35 @@ def app():
                     f = pd.read_csv(file, sep=';', encoding='cp1251')
                     #-- Заполнение зависимого и независимого вектора --#
                     yy = np.asarray(f['Y'])
-                    xx = []
-                    for i in range(values['-PARAM_NUM_1-']):
-                        name = 'x'+str(i+1)
-                        xx.append([])
-                        xx[i].extend(np.asarray(f[name]))
-                    xx = np.array(xx)
-                    #-- Заполнение параметров --#
-                    const_n = values['-PARAM_NUM_1-']
-                    degree = values['-DEGREE_1-']
-                    window_size = values['-WIN_SIZE_1-']
-                    # передаваемые параметры:
-                    # 1) Многомерный массив хх (каждая строка массива - выборка)
-                    # 2) Одномерный массив yy
-                    # 3) Степень полинома
-                    # 4) Число параметров, от которых зависит вектор уу
-                    # 5) Окно (доля выборки которая будет явлться окном)
-                    x_new, y_new, eps = loess.MultidimLOESS(xx, yy, degree, const_n, window_size)
+                    xx_new = []
+                    yy_new = []
+                    eps_new = []
+                    k=0
+                    for j in active_windows:
+                        xx = []
+                        if(values[('-PARAM_NUM_-', j)]):
+                            for i in range(values[('-PARAM_NUM_-', j)]):
+                                name = 'x'+str(i+1)
+                                xx.append([])
+                                xx[i].extend(np.asarray(f[name]))
+                            xx = np.array(xx)
+                            #-- Заполнение параметров --#
+                            const_n = values[('-PARAM_NUM_-', j)]
+                            degree = values[('-DEGREE_-', j)]
+                            window_size = values[('-WIN_SIZE_-', j)]
+                            # передаваемые параметры:
+                            # 1) Многомерный массив хх (каждая строка массива - выборка)
+                            # 2) Одномерный массив yy
+                            # 3) Степень полинома
+                            # 4) Число параметров, от которых зависит вектор уу
+                            # 5) Окно (доля выборки которая будет явлться окном)
+                            x_new, y_new, eps = loess.MultidimLOESS(xx, yy, degree, const_n, window_size)
+                            xx_new.append([])
+                            yy_new.append([])
+                            xx_new[k].extend(x_new[0])
+                            yy_new[k].extend(y_new)
+                            eps_new.append(eps)
+                            k+=1
                     #-- Вывод результатов --#
                     window.hide()
                     layout_result = [[sg.Menubar(menu_def_second, tearoff=False)],
@@ -110,7 +135,7 @@ def app():
                                      [sg.Button('Сохранить изображение', key='-OUT_IMG-')],
                                      [sg.Button('Выход', key='-CANCEL_RES-')]]
                     window_result = sg.Window('Window Title', layout_result, finalize=True)
-                    draw_figure(window_result['-CANVAS-'].TKCanvas, create_plot(xx, yy, x_new, y_new))
+                    draw_figure(window_result['-CANVAS-'].TKCanvas, create_plot(xx, yy, xx_new, yy_new))
                     #-- Цикл для обработки "событий" и получения "значений" входных данных --#
                     while True:
                         event, values = window_result.read()
